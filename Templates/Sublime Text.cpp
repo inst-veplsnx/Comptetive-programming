735173e9df331bc17fa31aca547160f5c21eba00
Kurmankul Nurislam <kurn9367@gmail.com>
2025-03-05 19:53:48 +0500

    Add files via upload

---
+++ Templates/Sublime Text.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define u128 unsigned __int128
+#define i128 __int128
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define eb emplace_back
+#define mt make_tuple
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define fi first
+#define se second
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << ' ' << x << '\n';
+#else
+#define debug(x)
+#endif
+
+const ll inf = 9e18;
+const int iinf = 2e9;
+const int N = 2e5 + 20;
+
+void solution()
+{
+    
+}
+
+signed main(/* Kurmankul Nurislam */)
+{
+    #ifndef ONLINE_JUDGE
+        freopen("Input.txt", "r", stdin);
+        freopen("Output.txt", "w", stdout);
+        freopen("Errors.txt", "w", stderr);
+    #endif
+    cin.tie(nullptr) -> sync_with_stdio(false);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+        cout << '\n';
+    }
+    cerr << "Omir kayf\n";
+    #ifndef ONLINE_JUDGE
+        cout << "\nTime elapsed: " << double(clock()) / CLOCKS_PER_SEC << " s.\n";
+    #endif
+}
---
+++ Templates/Visual Studio 2017.cpp
@@ -0,0 +1,69 @@
+#pragma warning(disable : 4996)
+#include <condition_variable>
+#include <initializer_list>
+#include <unordered_map>
+#include <unordered_set>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <string_view>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <complex>
+#include <climits>
+#include <cmath>
+#include <cassert>
+#include <map>
+#include <set>
+#include <atomic>
+#include <future>
+#include <array>
+#include <tuple>
+#include <regex>
+#include <cfloat>
+#include <numeric>
+#include <cctype>
+#include <random>
+#include <mutex>
+#include <chrono>
+#include <bitset>
+#include <cstdint>
+#include <functional>
+#include <fstream>
+#include <queue>
+#include <deque>
+#include <string>
+#include <cstring>
+
+#define ll long long
+
+using namespace std;
+
+const ll inf = 9e18;
+const int iinf = 2e9;
+
+
+void solution()
+{
+    
+}
+
+int main(/* Kurmankul Nurislam*/)
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+    /*FILE* stream_in = freopen("trains.in", "r", stdin);
+    FILE* stream_out = freopen("trains.out", "w", stdout);
+    if (stream_in == NULL || stream_out == NULL)
+        return 0;
+    */
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
